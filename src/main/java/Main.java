f893eddcabed94e71601d6cc35e5e05ae6e51fd3
PiotrBoKar <piotr.bober0@gmail.com>
2023-12-04 21:51:17 +0100

    initial comm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int checkPt1 = ReadFile.getGameList(ReadFile.getList("23D2"));
-        int checkPt2 = ReadFile.getLargestList(ReadFile.getList("23D2"));
-        System.out.println(checkPt1);
-        System.out.println(checkPt2);
+        System.out.println(ReadFile.getGameList(ReadFile.getList("23D2")));
+        System.out.println(ReadFile.getLargestList(ReadFile.getList("23D2")));
 
     }
 }
